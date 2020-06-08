bf1dffd6396849762c61e7754a9dbcb6cf144191
나민진 <nmj8585@gmail.com>
2020-06-08 15:12:02 +0900

    기존 파일 옴기기했던 잘못된 파일 삭제 및 정리

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -9,7 +9,8 @@
 </template>
 
 <script>
-  import TodoHeader from './components/TodoHeader.vue'
+  //다른곳에 생성된 뷰 파일을 불러와 사용하게 함. 
+  import TodoHeader from './components/TodoHeader.vue'//
   import TodoInput from './components/TodoInput.vue'
   import TodoList from './components/TodoList.vue'
   import TodoFooter from './components/TodoFooter.vue'
